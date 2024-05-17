d2e223d6b04ba11eeeb0121c50cb1ea1007ae201
Mireeek <arfix123@gmail.com>
2024-05-17 15:38:20 +0200

    Chat GPT4o timing

--- mini1.cpp
+++ mini1.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <chrono>
 
-void manipulate_queue(auto queue){
+// Function to manipulate the queue
+template <typename Queue>
+void manipulate_queue(Queue& queue){
     for(int i = 0; i < 1000000; i+=5){
         queue.enqueue(i);
         queue.enqueue(i+1);
@@ -13,8 +15,23 @@ void manipulate_queue(auto queue){
 }
 
 int main(int argc, const char** argv) {
-    volatile Student::queue<int> free_lock_queue;
-    volatile Student::posix_queue<int> posix_queue;
+    // Volatile keyword is removed because it's unnecessary for this context
+    Student::queue<int> free_lock_queue;
+    Student::posix_queue<int> posix_queue;
 
-    
+    // Measure time for free_lock_queue
+    auto start_free_lock = std::chrono::high_resolution_clock::now();
+    manipulate_queue(free_lock_queue);
+    auto end_free_lock = std::chrono::high_resolution_clock::now();
+    auto duration_free_lock = std::chrono::duration_cast<std::chrono::milliseconds>(end_free_lock - start_free_lock).count();
+    std::cout << "Free-lock queue manipulation time: " << duration_free_lock << " ms" << std::endl;
+
+    // Measure time for posix_queue
+    auto start_posix = std::chrono::high_resolution_clock::now();
+    manipulate_queue(posix_queue);
+    auto end_posix = std::chrono::high_resolution_clock::now();
+    auto duration_posix = std::chrono::duration_cast<std::chrono::milliseconds>(end_posix - start_posix).count();
+    std::cout << "POSIX queue manipulation time: " << duration_posix << " ms" << std::endl;
+
+    return 0;
 }
